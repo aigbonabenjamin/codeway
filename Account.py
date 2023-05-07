564c58268a620ef16e11cf883ff40b7f3c785c8c
aigbonabenjamin <aigbonabenjamin@gmail.com>
2023-05-08 00:46:40 +0100

    oop

---
+++ Account.py
@@ -0,0 +1,12 @@
+class Account:
+    name = "John Dave "
+    account_number = 555777442
+    account_balance = 10000
+
+def __init__(self):
+    self.account_balance()
+    self.account_number()
+
+def deposit(self,balance):
+    self.balance = self.balance
+    self.balance  = self.balance-self
